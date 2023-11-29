f5b1fbaa432f331184a21e0e81389c44682bba1e
markej1 <xmjarx@gmail.com>
2023-11-29 14:26:56 +0100

    shared preferences added

--- app/src/main/java/com/example/zpi_mobile/MainActivity.kt
+++ app/src/main/java/com/example/zpi_mobile/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,10 +43,15 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun StartScreen() {
-        val visible1 by remember { mutableStateOf(true) }
         var visible2 by remember { mutableStateOf(false) }
         var visible3 by remember { mutableStateOf(false) }
         var visible4 by remember { mutableStateOf(false) }
+
+        val sharedPreferencesManager = SharedPreferencesManager(LocalContext.current)
+        sharedPreferencesManager.saveData("level", "")
+        sharedPreferencesManager.saveData("field", "")
+        sharedPreferencesManager.saveData("cycle", "")
+        sharedPreferencesManager.saveData("specialization", "")
 
         Scaffold(
 //        topBar = {
@@ -63,31 +69,41 @@ class MainActivity : ComponentActivity() {
                     Column {
                         StartProgramChoice(
                             text = "Wybierz stopień studiów",
-                            visibility = visible1,
+                            visibility = true,
+                            key = "level",
                             onClick = {
                                 visible2 = true
                                 visible3 = false
                                 visible4 = false
+                                sharedPreferencesManager.saveData("field", "")
+                                sharedPreferencesManager.saveData("cycle", "")
+                                sharedPreferencesManager.saveData("specialization", "")
                             }
                         )
                         StartProgramChoice(
                             text = "Wybierz kierunek studiów",
                             visibility = visible2,
+                            key = "field",
                             onClick = {
                                 visible3 = true
                                 visible4 = false
+                                sharedPreferencesManager.saveData("cycle", "")
+                                sharedPreferencesManager.saveData("specialization", "")
                             }
                         )
                         StartProgramChoice(
                             text = "Wybierz cykl kształcenia",
                             visibility = visible3,
+                            key = "cycle",
                             onClick = {
                                 visible4 = true
+                                sharedPreferencesManager.saveData("specialization", "")
                             }
                         )
                         StartProgramChoice(
                             text = "Wybierz specjalność",
                             visibility = visible4,
+                            key = "specialization",
                             onClick = {
 
                             }
@@ -100,13 +116,14 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun StartProgramChoice(
-        text: String = "M",
+        text: String,
         visibility: Boolean? = false,
         possibilities: List<String> = listOf("IST", "IST - ang.", "IS", "IZ", "Z"),
+        key: String,
         onClick: () -> Unit
     ) {
         if (visibility == true) {
-            var selectedText by remember { mutableStateOf("") }
+            val sharedPreferencesManager = SharedPreferencesManager(LocalContext.current)
             var expanded by remember { mutableStateOf(false) }
             val icon = if (expanded) {
                 Icons.Filled.KeyboardArrowUp
@@ -121,8 +138,10 @@ class MainActivity : ComponentActivity() {
                     .padding(20.dp)
             ) {
                 OutlinedTextField(
-                    value = selectedText,
-                    onValueChange = { selectedText = it },
+                    value = sharedPreferencesManager.getData(key, ""),
+                    onValueChange = {
+                        sharedPreferencesManager.saveData(key, it)
+                    },
                     label = { Text(text) },
                     modifier = Modifier.onGloballyPositioned { layoutCoordinates ->
                         outlineTextFieldSize = layoutCoordinates.size.toSize()
@@ -148,7 +167,7 @@ class MainActivity : ComponentActivity() {
                         DropdownMenuItem(
                             text = { Text(possibility) },
                             onClick = {
-                                selectedText = possibility
+                                sharedPreferencesManager.saveData(key, possibility)
                                 expanded = false
                                 onClick()
                             }
