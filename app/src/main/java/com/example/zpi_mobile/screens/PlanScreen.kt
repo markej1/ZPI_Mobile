3c6618b043e841bf05ee3d10350a4351bb3670e9
markej1 <xmjarx@gmail.com>
2023-12-02 19:33:47 +0100

    page information

--- app/src/main/java/com/example/zpi_mobile/screens/PlanScreen.kt
+++ app/src/main/java/com/example/zpi_mobile/screens/PlanScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,9 +17,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun PlanScreen() {
     val semesters = listOf(
@@ -32,6 +34,10 @@ fun PlanScreen() {
         "Semestr 6.",
         "Semestr 7."
     )
+
+    val pagerState = rememberPagerState()
+    val semester = semesters[pagerState.currentPage]
+    val scope = rememberCoroutineScope()
 
     Column() {
 
@@ -63,21 +69,39 @@ fun PlanScreen() {
                     horizontalArrangement = Arrangement.SpaceEvenly,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowLeft,
-                            contentDescription = "left"
-                        )
+                    IconButton(
+                        enabled = pagerState.currentPage != 0,
+                        onClick = {
+                            scope.launch {
+                                pagerState.scrollToPage(pagerState.currentPage - 1)
+                            }
+                        }
+                    ) {
+                        if (pagerState.currentPage != 0) {
+                            Icon(
+                                imageVector = Icons.Default.KeyboardArrowLeft,
+                                contentDescription = "left"
+                            )
+                        }
                     }
                     Text(
-                        text = "Wszystko",
+                        text = semester,
                         fontSize = 24.sp
                     )
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowRight,
-                            contentDescription = "right"
-                        )
+                    IconButton(
+                        enabled = pagerState.currentPage != semesters.size - 1,
+                        onClick = {
+                            scope.launch {
+                                pagerState.scrollToPage(pagerState.currentPage + 1)
+                            }
+                        }
+                    ) {
+                        if (pagerState.currentPage != semesters.size - 1) {
+                            Icon(
+                                imageVector = Icons.Default.KeyboardArrowRight,
+                                contentDescription = "right"
+                            )
+                        }
                     }
                 }
             }
@@ -87,6 +111,7 @@ fun PlanScreen() {
         Box(modifier = Modifier.fillMaxSize()) {
             HorizontalPager(
                 count = semesters.size,
+                state = pagerState,
                 modifier = Modifier.fillMaxSize()
             ) { index ->
                 Text(text = semesters[index])
