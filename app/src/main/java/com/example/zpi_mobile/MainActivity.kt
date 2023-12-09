27cc19f91756336ecbb5c8c0ac643b832b6e34be
markej1 <xmjarx@gmail.com>
2023-12-09 12:18:08 +0100

    small fixes after merge

--- app/src/main/java/com/example/zpi_mobile/MainActivity.kt
+++ app/src/main/java/com/example/zpi_mobile/MainActivity.kt
@@ -5,8 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import com.example.zpi_mobile.navigation.Navigation
-import com.example.zpi_mobile.screens.PlanScreen
-import com.example.zpi_mobile.services.SubjectService
+import com.example.zpi_mobile.http.receive.SubjectService
 import com.example.zpi_mobile.ui.theme.ZPIMobileTheme
 
 
@@ -15,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ZPIMobileTheme() {
+            ZPIMobileTheme {
                 Navigation(viewModel)
             }
         }
